806feb500c8b88b551e26db78e6cb732f2166373
Sri Gunnala <srikanth.gunnala@gmail.com>
2019-08-12 20:24:30 +1000

    comments

--- XboxGame/RateGameViewModel.cs
+++ XboxGame/RateGameViewModel.cs
@@ -1,38 +1,66 @@
 ﻿using Caliburn.Micro;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls.Primitives;
 using XboxGame.Interface;
 using XboxGame.Models;
 
 namespace XboxGame
 {
+    /// <summary>
+    /// View Model class to handle game reviews
+    /// </summary>
     public class RateGameViewModel : Screen 
     {
+        /// <summary>
+        /// Event Aggregator object
+        /// </summary>
         private readonly IEventAggregator _eventAggregator;
 
-        private IGameService _gameService;
+        /// <summary>
+        /// Game service object
+        /// </summary>
+        private readonly IGameService _gameService;
+
+        /// <summary>
+        /// Gets or sets the value of Game object
+        /// </summary>
         public Game Game { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value of game ratings
+        /// </summary>
         public List<GameRating> GameRatings { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value of selected game ratings
+        /// </summary>
         public GameRating SelectedGameRating { get; set; }
 
+        /// <summary>
+        /// Gets or sets the value of revview comments
+        /// </summary>
         public string ReviewComments { get; set; }
 
+        /// <summary>
+        /// Instantiates the new object of RateGameViewModel
+        /// </summary>
+        /// <param name="gameService">game service object</param>
+        /// <param name="eventAggregator">event aggregator object</param>
+        /// <param name="game"></param>
         public RateGameViewModel(IGameService gameService, IEventAggregator eventAggregator, Game game)
         {
+            // Initialization
             this._eventAggregator = eventAggregator;
             this._gameService = gameService;
             this.Game = game;
 
+            // Load game rating definitions.
             this.GameRatings = this._gameService.GetGameRatingDef();
             this.SelectedGameRating = this.GameRatings.Find(r => r.Id == 1);
         }
 
+        /// <summary>
+        /// Submits the review comments for game
+        /// </summary>
         public void RateGame()
         {
             GameReview review = new GameReview()
@@ -44,10 +72,14 @@ namespace XboxGame
 
             this._gameService.PostReview(review);
 
+            // close the pop up and load game list view
             TryClose();
             LoadGameList();
         }
 
+        /// <summary>
+        /// Load game list view
+        /// </summary>
         public void LoadGameList()
         {
             EventMessage target = new EventMessage();
