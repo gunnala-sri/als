e5c49c8f1632c57f0bbd78e87bebbf48b4e24d3f
Sri Gunnala <srikanth.gunnala@gmail.com>
2019-08-12 20:31:47 +1000

    added comments

--- XboxGame/ShellViewModel.cs
+++ XboxGame/ShellViewModel.cs
@@ -2,30 +2,50 @@ using Caliburn.Micro;
 using XboxGame.Interface;
 using XboxGame.Models;
 
-namespace XboxGame {
+namespace XboxGame
+{
+    /// <summary>
+    /// The main parent shell view model class for the application
+    /// </summary>
     public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell,  IHandle<EventMessage>
     {
+        /// <summary>
+        /// Event aggregator object
+        /// </summary>
         private readonly IEventAggregator _eventAggregator;
 
-        public object GameListVM { get; set; }
+        /// <summary>
+        /// Gets or sets the value of content view to be displayed
+        /// </summary>
+        public object ConentView { get; set; }
 
+        /// <summary>
+        /// Instantiates the new object of shell view model class
+        /// </summary>
+        /// <param name="eventAggregator">event aggregator object</param>
         public ShellViewModel(IEventAggregator eventAggregator)
         {
+            //Initialization
             _eventAggregator = eventAggregator;
             _eventAggregator.Subscribe(this);
 
-            GameListVM = new GameListViewModel(IoC.Get<IGameService>(), _eventAggregator);
+            // Set the view
+            ConentView = new GameListViewModel(IoC.Get<IGameService>(), _eventAggregator);
         }
 
+        /// <summary>
+        /// Event aggregator handler method
+        /// </summary>
+        /// <param name="message">Handler message object</param>
         public void Handle(EventMessage message)
         {
             if (message.Message == "Details")
             {
-                GameListVM = new GameDetailViewModel(IoC.Get<IGameService>(), _eventAggregator, message.Data as Game );
+                ConentView = new GameDetailViewModel(IoC.Get<IGameService>(), _eventAggregator, message.Data as Game );
             }
             else
             {
-                GameListVM = new GameListViewModel(IoC.Get<IGameService>(), _eventAggregator);
+                ConentView = new GameListViewModel(IoC.Get<IGameService>(), _eventAggregator);
             }
         }
     }
